2fb7070a8af6878debaf2f9467ce55f977c52018
Forbes Lindesay <forbes@lindesay.co.uk>
2017-05-21 14:06:07 +0100

    Use helper methods for e2e test

--- src/end-to-end-tests/index.js
+++ src/end-to-end-tests/index.js
@@ -32,23 +32,7 @@ getPort().then(port => {
 
       // allow 5 seconds for the element to appear
       const timeout = Date.now() + 5000;
-      let story = null;
-      while (story === null && Date.now() < timeout) {
-        const stories = driver.browser.activeWindow.getElements(
-          '[data-test-id="story"]',
-        );
-        stories.some(storyElement => {
-          if (
-            storyElement
-              .getElement('[data-test-id="StoryBody"]')
-              .getText()
-              .includes('TEST:' + id)
-          ) {
-            story = storyElement;
-            return true;
-          }
-        });
-      }
+      const story = waitFor(() => getStoryWithText('TEST:' + id));
       if (story === null) {
         throw new Error('Story was not created');
       }
@@ -58,13 +42,12 @@ getPort().then(port => {
       );
       story.getElement('[data-test-id="AddVote"]').mouse.click();
 
-      const voteTimeout = Date.now() + 5000;
-      while (
-        Date.now() < voteTimeout &&
-        story.getElement('[data-test-id="VoteCount"]').getText().trim() !== '1'
-      );
       assert(
-        story.getElement('[data-test-id="VoteCount"]').getText().trim() === '1',
+        waitFor(
+          () =>
+            story.getElement('[data-test-id="VoteCount"]').getText().trim() ===
+            '1',
+        ),
         'Expect vote count to be 1 after clicking on the element',
       );
     } finally {
@@ -73,5 +56,37 @@ getPort().then(port => {
       serverProcess.kill();
     }
     process.exit(0);
+
+    function getStoryWithText(text) {
+      // there's no way to select an element with some given text, so select all the stories
+      const stories = driver.browser.activeWindow.getElements(
+        '[data-test-id="story"]',
+      );
+      for (const story of stories) {
+        // for each of the stories, check to see if it includes the text we're checking for
+        if (
+          story
+            .getElement('[data-test-id="StoryBody"]')
+            .getText()
+            .includes(text)
+        ) {
+          return story;
+        }
+      }
+      return null;
+    }
+
+    // retry a function until it returns something truthy
+    // by default, this will wait up to 5000
+    function waitFor(fn, timeout = 5000) {
+      const timeoutEnd = Date.now() + timeout;
+      while (Date.now() < timeout) {
+        const value = fn();
+        if (value) {
+          return value;
+        }
+      }
+      return fn();
+    }
   });
 });
